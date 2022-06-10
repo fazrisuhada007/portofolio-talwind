51d9e4f1a1350b6e96b04deb3831d2f3ee426729
fazrisuhada007 <fazrisuhada3@gmail.com>
2022-06-10 13:49:26 +0900

    portofolio rizuka homade

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      colors: {
+        primary: '#B91C1C',
+        dark: '#0F172A',
+        secondary: '#1E293B'
+      },
+      screens: {
+        '2xl': '1320px'
+      }
+    },
+  },
+  plugins: [],
+}
